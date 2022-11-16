9120fb354e0edbd2cd2b82c7f55dd407dc098266
khaleelankoush <khaleel.ankoush@gmail.com>
2022-11-16 14:03:10 +0300

    edits

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const https = require('https');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -59,8 +61,8 @@ app.post('/failure', function (req, res) {
   res.redirect('/');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Server is running on port 3000');
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // API Key
